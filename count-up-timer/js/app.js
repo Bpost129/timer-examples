95b0cc08e079e85d44a859c32ebc8b2011650c9b
Bpost129 <Bpost129@yahoo.com>
2025-12-03 22:58:34 -0500

    create tick function to change changes and render time

--- count-up-timer/js/app.js
+++ count-up-timer/js/app.js
@@ -1,3 +1,6 @@
+let timerIntervalId
+let winTime, min, sec, seconds = 0
+
 const timerEl = document.getElementById('timer')
 const winBtn = document.getElementById('win-button')
 const winMsg = document.getElementById('message')
@@ -6,5 +9,24 @@ const resetBtn = document.getElementById('reset-button')
 winBtn.addEventListener('click', handleClickWin)
 resetBtn.addEventListener('click', startTimer)
 
+function startTimer() {
 
+}
+
+function handleClickWin() {
+
+}
+
+function tick() {
+  seconds++
+  renderTime()
+}
+
+function renderMessage() {
+
+}
+
+function renderTime() {
+
+}
 
