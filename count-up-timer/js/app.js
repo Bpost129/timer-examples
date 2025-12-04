f9d188144c07a3508252ce603d0be665387d6ca4
Bpost129 <Bpost129@yahoo.com>
2025-12-03 22:52:26 -0500

    attach event listeners to buttons

--- count-up-timer/js/app.js
+++ count-up-timer/js/app.js
@@ -2,3 +2,9 @@ const timerEl = document.getElementById('timer')
 const winBtn = document.getElementById('win-button')
 const winMsg = document.getElementById('message')
 const resetBtn = document.getElementById('reset-button')
+
+winBtn.addEventListener('click', handleClickWin)
+resetBtn.addEventListener('click', startTimer)
+
+
+
