061b013996c90fcbf4f61e6592fe0fc3644dba05
Bpost129 <Bpost129@yahoo.com>
2025-12-03 22:43:46 -0500

    create a basic timer and render each second followed by a message

--- count-down-timer/js/app.js
+++ count-down-timer/js/app.js
@@ -1,1 +1,12 @@
 let countdownEl = document.getElementById('countdown')
+
+let timeLeft = 10
+
+let timer = setInterval(() => {
+  countdownEl.textContent = timeLeft + ' seconds remaining'
+  timeLeft -= 1
+  if (timeLeft < 0) {
+    countdownEl.textContent = 'Finished!'
+    confetti.start(500)
+  }
+}, 1000)
